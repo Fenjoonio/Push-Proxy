c1f18c99491aef8cb5675aa1eb6bc861d70dd600
Themmr <you@example.com>
2025-05-10 21:13:56 +0330

    update push

--- internals/handlers/push.go
+++ internals/handlers/push.go
@@ -17,8 +17,36 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PushHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Push recieved")
+	client := &http.Client{}
 
+	req, err := http.NewRequest(r.Method, expoSendAPI, r.Body)
+	if err != nil {
+		http.Error(w, "Failed to create request", http.StatusInternalServerError)
+		return
+	}
+
+	for name, values := range r.Header {
+		for _, value := range values {
+			req.Header.Add(name, value)
+		}
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Failed to contact API", http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+	fmt.Fprintf(w, "Push recieved")
 }
 
 func createProxyHandler(targetURL string) http.HandlerFunc {
