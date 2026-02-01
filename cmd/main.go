721f76d86f0a259f63187ee157d26d2a2c7e9775
Yasin Silavi <ys.silavi@gmail.com>
2026-02-01 17:14:18 +0330

    update

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/freakingeek/phoxy/internals/routes"
 )
 
-
-
 func main() {
 	routes.SetupRoutes()
 
-	log.Println("Proxy server running on :3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	addr := "0.0.0.0:" + port
+	log.Printf("Proxy server running on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
